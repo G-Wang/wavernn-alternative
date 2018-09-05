d7e3d8c879f0bf5057a06e6d2d964b10ff3f1f47
G-Wang <ywa289@sfu.ca>
2018-09-05 10:26:29 -0700

    update to include deepvoice

---
+++ utils/hparams.py
@@ -0,0 +1,15 @@
+class hparams:
+    # audio
+    num_mels = 80
+    fmin = 125
+    fmax = 7600
+    fft_size = 2048
+    hop_size = 256
+    sample_rate = 22050
+    preemphasis=0.97
+    min_level_db = -100
+    ref_level_db = 20
+    rescaling=False
+    rescaling_max=0.999
+    allow_clipping_in_normalization=True
+
